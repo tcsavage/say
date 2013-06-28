69a27b61639a46a33adb162367ede781943274b6
tcsavage <tcsavage@gmail.com>
2013-06-28 15:55:05 +0100

    More robust OSX implementation

--- foreign/TTS-osx.cpp
+++ foreign/TTS-osx.cpp
@@ -3,8 +3,26 @@
 #include <string.h>
 #include "TTS.h"
 
+#include <Carbon/Carbon.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <ApplicationServices/ApplicationServices.h>
+
+static SpeechChannel sc;
+static bool speaking;
+static Fixed volume;
+
+static void speechDone(SpeechChannel sc2, void *refcon) {
+	if (sc2 == sc)
+	{
+		DisposeSpeechChannel(sc);
+		speaking = false;
+	}
+}
+
 TTS::TTS()
 {
+	speaking = false;
+	setVolume(100);
 }
 
 TTS::~TTS()
@@ -13,14 +31,18 @@ TTS::~TTS()
 
 void TTS::say(const char * words)
 {
-	int n = strlen(words);
-	char * command = new char[n+4];
-	strcat(command, "say ");
-	strcat(command, words);
-	system(command);
-	printf("SAY: %s\n", words);
+	if (!speaking)
+	{
+		NewSpeechChannel(NULL, &sc);
+		SetSpeechInfo(sc, soVolume, &volume);
+		SetSpeechInfo(sc, soSpeechDoneCallBack, reinterpret_cast<void *>(speechDone));
+		CFStringRef cfstr = CFStringCreateWithCString(NULL, words, kCFStringEncodingUTF8);
+		OSErr err = SpeakCFString(sc, cfstr, NULL);
+		speaking = true;
+	}
 }
 
 void TTS::setVolume(int v)
 {
+	volume = FixRatio(v, 100);
 }
