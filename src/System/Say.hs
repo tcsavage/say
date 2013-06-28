f01c92a2454966c602a06cba29177419daaa3167
Tom Savage <tcsavage@gmail.com>
2013-06-28 19:44:14 +0100

    Added some documentation. Removed withVolume

--- src/System/Say.hs
+++ src/System/Say.hs
@@ -5,11 +5,11 @@ import Foreign.C
 import Foreign.C.String
 import System.Say.FFI
 
+-- | Send a string to the TTS system.
 say :: String -> IO ()
 say = csay <=< newCString
 
-setVolume :: Int -> IO ()
+-- | Set the current volume of the TTS system.
+setVolume :: Int -- ^ Value between 0 and 100
+          -> IO ()
 setVolume = csetVolume . toEnum
-
-withVolume :: Int -> IO a -> IO a
-withVolume = undefined
